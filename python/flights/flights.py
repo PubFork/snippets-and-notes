3178497f4777c78071bc51abde9878fece6ba76e
Matt Schuchard <grapheneowns@gmail.com>
2019-07-22 21:43:19 -0400

    refine filtering and expand inputs

--- python/flights/flights.py
+++ python/flights/flights.py
@@ -1,24 +1,33 @@
-#TODO: variable destination, variable departure date, store response data in file and check timestamp for update, track prices over time, number/code/duration/availability might be nice, sort by price, https://amadeus4dev.github.io/amadeus-python/#shopping-hotels
+#TODO: interface for inputs (see constants below), kv of dest-depart for matrix of possibilities, store response data in file and check timestamp for update, track prices over time, availability might be nice, https://amadeus4dev.github.io/amadeus-python/#shopping-hotels
 
+import re
 from amadeus import Client
 import ruamel.yaml as yaml
 
 
+ORIGIN = 'ATL'
+DEST = 'MIA'
+DATE = '2019-12-23'
+DEBUG = True
+
 # set credentials
 creds = yaml.safe_load(open('/home/matt/.amadeus/creds').read())
 
-# initialize client
-amadeus = Client(client_id=creds['api_key'], client_secret=creds['api_secret'])
+if DEBUG:
+    data = open('debug.txt').read().to_dict()
+else:
+    # initialize client
+    amadeus = Client(client_id=creds['api_key'], client_secret=creds['api_secret'])
 
-# configure request and assign response
-response = amadeus.shopping.flight_offers.get(
-    origin='ATL',
-    destination='MIA',
-    departureDate='2019-12-23'
-)
+    # configure request and assign response
+    response = amadeus.shopping.flight_offers.get(
+        origin=ORIGIN,
+        destination=DEST,
+        departureDate=DATE
+    )
 
-# store response data
-data = response.data
+    # store response data
+    data = response.data
 
 # initialize filtered array of offers
 filtered = []
@@ -29,6 +38,16 @@ for offer in data:
     if len(offer['offerItems'][0]['services'][0]['segments']) == 1 and offer['offerItems'][0]['services'][0]['segments'][0]['flightSegment']['carrierCode'] == 'DL':
         filtered.append(offer['offerItems'][0])
 
+# output origin, dest, date
+print('Origin:', ORIGIN, 'Destination:', DEST, 'Date:', DATE)
+
 # output time and price
 for offer in filtered:
-    print('Departure Time:', offer['services'][0]['segments'][0]['flightSegment']['departure']['at'], 'Price:', offer['price']['total'])
+    # easier to read
+    depart = offer['services'][0]['segments'][0]['flightSegment']['departure']['at']
+    depart_time = re.sub(r'-.*', '', re.sub(r'.*T', '', depart))
+    price = offer['price']['total']
+    number = offer['services'][0]['segments'][0]['flightSegment']['number']
+    plane = offer['services'][0]['segments'][0]['flightSegment']['aircraft']['code']
+    # output info
+    print('Number:', number, 'Departure Time:', depart_time, 'Price:', price, 'Plane:', plane)
